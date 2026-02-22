e48570e356ddc6b39b50c4ebabfc329f190f335a
Kitx-01 <kexinxu2001@gmail.com>
2026-02-22 21:46:56 +0000

    clean up unneeded files

--- example_run_gtd_ltd_only.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python prune_topic_graph.py \
-  --input seo-graph-10tools-single-5depth.json \
-  --out-filtered seo-graph-10tools-single-5depth-3filt.json \
-  --out-report report-seo-graph-10tools-single-5depth-3filt.json \
-  --model all-MiniLM-L6-v2 \
-  --seed 0 \
-  --depth1-gtd 0.25 --gtd-neg 0.0 --ltd 0.25
